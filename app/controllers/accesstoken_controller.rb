968a6c8351f38f2bbf3055fcfd64494cc79fc5c4
Dende <alegrria@gmail.com>
2017-09-29 01:58:50 +0200

    Redirecting to Twitter feed

--- app/controllers/accesstoken_controller.rb
+++ app/controllers/accesstoken_controller.rb
@@ -1,5 +1,18 @@
-class AccesstokenController < ApplicationController  
-  hash = { oauth_token: session[:token], oauth_token_secret: session[:token_secret]}
-  @request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
-  @access_token = @request_token.get_access_token
+class AccesstokenController < ApplicationController 
+
+  def prepare_access_token(oauth_token, oauth_verifier)
+      @consumer = OAuth::Consumer.new("b3shlPzonHvGNvGLcJmxIQRq9","uInEQ66GWBZMwoVBMQtZyeO6QlCBzfsL7kImf3G77j6hGhAoFJ", { :site => "https://api.twitter.com", :scheme => :header })
+      token_hash = { :oauth_token => oauth_token, :oauth_verifier => oauth_verifier }
+      access_token = OAuth::AccessToken.from_hash(@consumer, token_hash )
+
+      return access_token
+  end
+
+  def callback
+  	@request_token = session[:request_token]
+  	prepare_access_token(params[:oauth_token],params[:oauth_verifier])
+  	@consumer = OAuth::Consumer.new(params[:oauth_token],params[:oauth_verifier], :site => "https://api.twitter.com")
+    @access_token = prepare_access_token(params[:oauth_token],params[:oauth_verifier])
+  	redirect_to "https://api.twitter.com/1.1/statuses/Dende_Nu_timeline.json"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'login#index'
   get 'oauth', controller: :login, action: :oauth
-  get 'callback', controller: :login, action: :callback 
+  get 'callback', controller: :accesstoken, action: :callback 
 
 end
